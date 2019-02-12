31364f249418f836b6961f57368230987fd01f84
Natali Shkolna <nshkolna@gmail.com>
2019-02-12 12:05:08 +0200

    Add slide block. Fixed line after blocks

--- gulpfile.js
+++ gulpfile.js
@@ -46,6 +46,8 @@ gulp.task('styles:compile', function () {
 /* --------  js -------- */
 gulp.task('js', function() {
     return gulp.src([
+            'node_modules/jquery/dist/jquery.min.js',
+            'node_modules/bootstrap/dist/js/bootstrap.min.js',
             'source/js/init.js',
             'source/js/validation.js',
         ])
